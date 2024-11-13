2ab56c7c1c392c0d8bcc70907e4af8f83377899d
qinhgd <107462413+qinhgd@users.noreply.github.com>
2024-11-13 21:33:13 +0800

    Update AutoSetup.sh

--- AutoSetup.sh
+++ AutoSetup.sh
@@ -2,24 +2,24 @@
 clear
 set -euo pipefail
 echo "~~~~~~~~~~"
-echo "安装mosdns"
+echo "安装 mosdns"
 echo "~~~~~~~~~~"
 echo "下载依赖……"
 apt update
 apt install wget git unzip pip -y
 echo "克隆库……"
-git clone https://github.com/allanchen2019/mosdns-debian-install.git /opt/mosdns
+git clone https://github.com/allanchen2019/mosdns-debian-install.git /etc/mosdns
 chmod 777 -R /etc/mosdns
 echo "执行安装……"
 bash /etc/mosdns/install-mosdns.sh
 
-if systemctl status mosdns.service |grep -q "running"; then
-        echo "~~~~~~~~~~~~~~~~~~~~~~"
-        echo "安装完成，mosdns已运行！"
-        echo "~~~~~~~~~~~~~~~~~~~~~~"
-    else
-        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-        echo "Emm………好像哪里不太对，mosdns没运行………"
-        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+if systemctl status mosdns.service | grep -q "running"; then
+    echo "~~~~~~~~~~~~~~~~~~~~~~"
+    echo "安装完成，mosdns 已运行！"
+    echo "~~~~~~~~~~~~~~~~~~~~~~"
+else
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    echo "Emm………好像哪里不太对，mosdns 没运行………"
+    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 fi
-rm -rf ./AutoSetup.sh
+rm -rf./AutoSetup.sh
