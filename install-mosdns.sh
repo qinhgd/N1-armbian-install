13510396c7e72854f32913e64b06cdfe927ed873
qinhgd <107462413+qinhgd@users.noreply.github.com>
2024-11-13 21:38:33 +0800

    Update install-mosdns.sh

--- install-mosdns.sh
+++ install-mosdns.sh
@@ -5,7 +5,7 @@ architecture=$(dpkg --print-architecture)
 cd /etc/mosdns || exit
 mkdir bin
 cd bin || exit
-echo "下载mosdns和域名表……"
+echo "下载 mosdns 和域名表……"
 wget --show-progress -t 5 -T 10 -cqO mosdns.zip https://github.com/IrineSistiana/mosdns/releases/latest/download/mosdns-linux-"$architecture".zip
 wget --show-progress -t 5 -T 10 -cqO accelerated-domains.china.conf https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
 wget --show-progress -t 5 -T 10 -cqO apple.china.conf https://github.com/felixonmars/dnsmasq-china-list/raw/master/apple.china.conf
@@ -14,14 +14,12 @@ sed -r 's/.{16}$//' accelerated-domains.china.conf2 > accelerated-domains.china.
 sed -r 's/.{8}//' apple.china.conf > apple.china.conf2
 sed -r 's/.{16}$//' apple.china.conf2 > apple.china.conf.raw.txt
 
-echo "停用systemd-resolved……"
+echo "停用 systemd-resolved……"
 systemctl stop systemd-resolved.service
 systemctl disable systemd-resolved.service
 systemctl daemon-reload
-echo "启动mosdns……"
+echo "启动 mosdns……"
 unzip -o mosdns.zip
 ./mosdns service install -c /etc/mosdns/config-v5.yaml
 ./mosdns service start
 systemctl enable mosdns.service
-
-
