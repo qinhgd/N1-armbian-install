be142e29be8294db74e25eb699b3c7d57b7bda81
qinhgd <107462413+qinhgd@users.noreply.github.com>
2024-11-13 21:39:18 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,15 +2,15 @@
 #set -u
 clear
 echo "~~~~~~~~~~~~~~~~"
-echo "卸载mosdns………………"
+echo "卸载 mosdns………………"
 echo "~~~~~~~~~~~~~~~~"
 systemctl stop mosdns.service
 systemctl disable mosdns.service > /dev/null 2>&1
 rm /etc/systemd/system/mosdns.service
 systemctl daemon-reload
 systemctl reset-failed
-rm -rf /opt/mosdns 
-echo "重置DNS………………"
+rm -rf /etc/mosdns
+echo "重置 DNS………………"
 rm -rf /etc/resolv.conf
 cat << EOF >/etc/resolv.conf
 nameserver 1.1.1.1
